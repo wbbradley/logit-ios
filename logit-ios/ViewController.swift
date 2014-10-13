60dedaa8630afc338ae93e9130f4e0e974b64e8d
Will Bradley <williambbradley@gmail.com>
2014-10-13 00:47:13 -0700

    get some basic ui elements working, and get some boilerplate up and running for URL requests

--- logit-ios/ViewController.swift
+++ logit-ios/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, UITextViewDelegate {
 
 	var locationManager = CLLocationManager()
 	var labelDebug: UILabel!
@@ -22,8 +22,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 		buttonLogIt = UIButton()//frame: CGRectMake(0, 0, view.frame.size.width, 20))
 		labelDebug = UILabel()//frame: CGRectMake(0, 0, view.frame.size.width, 20))
 		textViewNotes = UITextView()//frame: CGRectMake(0, 0, view.frame.size.width, 20))
+		textViewNotes.delegate = self
 
 		buttonLogIt.setTitle("Log It", forState: UIControlState.Normal)
+		buttonLogIt.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
 
 		buttonLogIt.setTranslatesAutoresizingMaskIntoConstraints(false)
 		labelDebug.setTranslatesAutoresizingMaskIntoConstraints(false)
@@ -37,7 +39,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 		locationManager.desiredAccuracy = kCLLocationAccuracyBest
 		locationManager.requestWhenInUseAuthorization()
 		locationManager.startUpdatingLocation()
-
+		self.labelDebug.backgroundColor = UIColor.grayColor()
+		self.textViewNotes.backgroundColor = UIColor.grayColor()
+		self.buttonLogIt.backgroundColor = UIColor.grayColor()
 		var viewBindingsDict = NSMutableDictionary()
 		viewBindingsDict.setValue(labelDebug, forKey: "labelDebug")
 		viewBindingsDict.setValue(textViewNotes, forKey: "textViewNotes")
@@ -70,6 +74,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 			metrics: nil,
 			views: viewBindingsDict
 			))
+
+		buttonLogIt.addTarget(self, action: "findMyLocation:", forControlEvents: .TouchUpInside)
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -77,8 +83,46 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 		// Dispose of any resources that can be recreated.
 	}
 
+	var responseMsg : String?
+
 	@IBAction func findMyLocation(sender: AnyObject) {
 		labelDebug.text = textViewNotes.text
+		var request = NSMutableURLRequest(URL: NSURL(string: "http://wormhorse.com/logit/login"))
+		var session = NSURLSession.sharedSession()
+		request.HTTPMethod = "POST"
+
+		var params = ["email":"test", "password":"password"] as Dictionary
+
+		var err: NSError?
+		request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &err)
+		request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+		request.addValue("application/json", forHTTPHeaderField: "Accept")
+
+		var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+			println("Response: \(response)")
+			var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+			println("Body: \(strData)\n\n")
+			var err: NSError?
+			var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as NSDictionary
+			// json = {"response":"Success","msg":"User login successfully."}
+			if (err != nil) {
+				println(err!.localizedDescription)
+			} else {
+				var success = json["response"] as? String
+				println("Succes: \(success)")
+
+				if json["response"] as NSString == "Success"
+				{
+					println("Login Successfull")
+				}
+				self.responseMsg = json["msg"] as? String
+				dispatch_async(dispatch_get_main_queue(), {
+					self.textViewNotes.text = self.responseMsg
+				})
+
+			}
+		})
+		task.resume()
 	}
 
 	func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
